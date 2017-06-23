0b4cd1f77185d6f8c876ab623714ae9413a97cd5
Josh Nielsen <josh@joshontheweb.com>
2017-06-23 17:02:42 -0600

    added index file

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Upload = require('./Upload');
+
+var _Upload2 = _interopRequireDefault(_Upload);
+
+var _UploadStream = require('./UploadStream');
+
+var _UploadStream2 = _interopRequireDefault(_UploadStream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = { Upload: _Upload2.default, UploadStream: _UploadStream2.default };
